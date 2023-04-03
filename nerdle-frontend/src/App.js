ad140f3d1d3c30b09cea1f9b36071a842bb8a25e
william.paul <william.paul13@ed.cna.nl.ca>
2023-04-03 18:32:31 -0400

    Initial Commit For React Website files

---
+++ nerdle-frontend/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import MyNavbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <MyNavbar />
+      <h1>Welcome to my website!</h1>
+    </div>
+  );
+}
+
+export default App;
