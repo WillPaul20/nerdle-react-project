56c293d00afe767234088946cd6b1b975cc37de0
william.paul <william.paul13@ed.cna.nl.ca>
2023-04-16 13:16:18 -0400

    Statistics calculation and component implemented

--- nerdle-frontend/src/components/StatisticsButton.js
+++ nerdle-frontend/src/components/StatisticsButton.js
@@ -1,13 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { AppContext } from "../App";
 import { FaChartBar } from 'react-icons/fa';
 
 const StatisticsButton = () => {
     const { isOpen, setIsOpen } = useContext(AppContext);
+    const [gamesPlayed, setGamesPlayed] = useState(0);
+    const [gamesWon, setGamesWon] = useState(0);
+  
+    useEffect(() => {
+      // Read from sessionStorage on mount
+      const played = sessionStorage.getItem('gamesPlayed');
+      const won = sessionStorage.getItem('gamesWon');
+  
+      if (played) {
+        setGamesPlayed(parseInt(played));
+      }
+  
+      if (won) {
+        setGamesWon(parseInt(won));
+      }
+    }, []);
 
-  const handleClick = () => {
-    setIsOpen(true);
-  }
+    const handleClick = () => {
+      setIsOpen(true);
+    };
+  
+    const closeModal = () => {
+      // Write to sessionStorage before closing
+      sessionStorage.setItem('gamesPlayed', gamesPlayed.toString());
+      sessionStorage.setItem('gamesWon', gamesWon.toString());
+      setIsOpen(false);
+    };
 
   return (
     <>
@@ -18,10 +41,10 @@ const StatisticsButton = () => {
         <div className="modal-overlay">
         <div className="modal">
           <h2>Statistics</h2>
-          <p>Games played: 10</p>
-          <p>Games won: 5</p>
-          <p>Win rate: 50%</p>
-          <button onClick={() => setIsOpen(false)}>Close</button>
+          <p>Games played: {gamesPlayed}</p>
+          <p>Games won: {gamesWon}</p>
+          <p>Win rate: {gamesPlayed > 0 ? `${Math.round((gamesWon / gamesPlayed) * 100)}%` : '-'}</p>
+          <button onClick={closeModal}>Close</button>
         </div>
       </div>
       )}
